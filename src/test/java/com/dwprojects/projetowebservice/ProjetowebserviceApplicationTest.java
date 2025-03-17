8c34db86410d3a9d64f9ed01d58f1e78291a2c21
dione.evangelista <dione.evangelista@lecom.com.br>
2025-03-16 22:01:11 -0300

    Test for main class app

---
+++ src/test/java/com/dwprojects/projetowebservice/ProjetowebserviceApplicationTest.java
@@ -0,0 +1,15 @@
+package com.dwprojects.projetowebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ProjetowebserviceApplicationTest {
+
+    @Test
+    void main() {
+        ProjetowebserviceApplication.main(new String[] {});
+    }
+}
