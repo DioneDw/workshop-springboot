abb2dc562fe3e295201cfc3e4e4e6280fd492199
dione.evangelista <dione.evangelista@lecom.com.br>
2025-03-19 18:58:59 -0300

    CategoryService test method findAll

---
+++ src/test/java/com/dwprojects/projetowebservice/services/CategoryServiceTest.java
@@ -0,0 +1,60 @@
+package com.dwprojects.projetowebservice.services;
+
+import com.dwprojects.projetowebservice.entities.Category;
+import com.dwprojects.projetowebservice.entities.Order;
+import com.dwprojects.projetowebservice.repositories.CategoryRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class CategoryServiceTest {
+
+    public static final long ID = 1L;
+    public static final String NAME_CATEGORY = "Name Category";
+    @InjectMocks
+    private CategoryService service;
+
+    @Mock
+    private CategoryRepository repository;
+
+    private Category category;
+    private Optional<Category> optionalCategory;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        startCategory();
+    }
+
+    @Test
+    void whenFindAllThenReturnCategories() {
+        when(repository.findAll()).thenReturn(List.of(category));
+
+        List<Category> response = service.findAll();
+
+        verify(repository,times(1)).findAll();
+        assertNotNull(response);
+        assertEquals(1, response.size());
+
+        assertEquals(Category.class, response.get(0).getClass());
+        assertEquals(ID, response.get(0).getId());
+        assertEquals(NAME_CATEGORY, response.get(0).getName());
+    }
+
+    void startCategory(){
+        category = new Category(ID, NAME_CATEGORY);
+        optionalCategory = Optional.of(new Category(ID, NAME_CATEGORY));
+    }
+}
